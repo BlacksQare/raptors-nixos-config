dd147f8d70ffd22311e9a0ac35887daef26c172e
Bartosz Mazurkiewicz <bartbart2003@gmail.com>
2026-08-22 17:34:59 +0200

    Fix VCAN autostart in QEMU

--- specific-configs/vm.nix
+++ specific-configs/vm.nix
@@ -57,6 +57,10 @@
     };
   };
 
+  systemd.network.networks."10-can0" = {
+    matchConfig.Name = "can0";
+  };
+
   # OAK cam udev rules
   services.udev.extraRules = ''
     SUBSYSTEM=="usb", ATTRS{idVendor}=="03e7", MODE="0666"
